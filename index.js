11e166c6db8f35175ef84be048d023d3f0528c9d
Arief Yahya Prasetio <prasetioariefyahya@gmail.com>
2023-10-31 23:42:34 +0700

    commit 31 oct 2023

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 import express from 'express'
 
-// import { getNotes, getNote, createNote } from './database.js'
+import { getNotes, getNote, createNote } from './database.js'
 
 const app = express()
 
 app.use(express.json())
 
 app.get("/notes", async (req, res) => {
-  // const notes = await getNotes()
-  res.send("CEK AJA SIH!")
+  const notes = await getNotes()
+  res.send(notes)
 })
 
-// app.get("/notes/:id", async (req, res) => {
-//   const id = req.params.id
-//   const note = await getNote(id)
-//   res.send(note)
-// })
-//
-// app.post("/notes", async (req, res) => {
-//   const { title, contents } = req.body
-//   const note = await createNote(title, contents)
-//   res.status(201).send(note)
-// })
+app.get("/notes/:id", async (req, res) => {
+  const id = req.params.id
+  const note = await getNote(id)
+  res.send(note)
+})
+
+app.post("/notes", async (req, res) => {
+  const { title, contents } = req.body
+  const note = await createNote(title, contents)
+  res.status(201).send(note)
+})
 
 app.use((err, req, res, next) => {
   console.error(err.stack)
